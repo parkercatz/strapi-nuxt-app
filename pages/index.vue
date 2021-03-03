e841fd4953d46af9a617c8b6b908a49f6453c445
ParkerCatz <tksm.webdesign09@gmail.com>
2021-03-03 23:00:05 +0900

    add: strapiで設定したものを表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,33 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">strapi-nuxt-app</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div v-if="error">
+      {{ error }}
     </div>
+    <ul v-else>
+      <li v-for="restaurant in restaurants" :key="restaurant.id">
+        {{ restaurant.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'App',
+  data() {
+    return {
+      restaurants: [],
+      error: null,
+    }
+  },
+  async mounted() {
+    try {
+      this.restaurants = await this.$strapi.$restaurants.find()
+    } catch (error) {
+      this.error = error
+    }
+  },
+}
 </script>
 
 <style>
@@ -37,27 +38,5 @@ export default {}
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
