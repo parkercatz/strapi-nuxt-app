112db042471495ee8fad19ee4e9d8bcb8e716a44
ParkerCatz <tksm.webdesign09@gmail.com>
2021-03-06 20:49:35 +0900

    add: Footerコンポーネントの追加

---
+++ components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-footer
+    >created by ParkerCatz. ©{{ copyright }} .All Rights Reserved.</v-footer
+  >
+</template>
+<script>
+export default {
+  data() {
+    return {
+      copyright: new Date().getFullYear(),
+    }
+  },
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,16 @@
   <v-app>
     <Header />
     <Nuxt />
+    <Footer />
   </v-app>
 </template>
 <script>
 import Header from '~/components/Header.vue'
+import Footer from '~/components/Footer.vue'
 export default {
   components: {
     Header,
+    Footer,
   },
 }
 </script>
